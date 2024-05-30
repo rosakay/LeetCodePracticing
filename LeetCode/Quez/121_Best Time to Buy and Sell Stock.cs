34c83d395e4af9155a0e94f4f9043c8b234eb93f
onekwei <rosakay11@gmail.com>
2024-05-30 11:48:15 +0800

    創建 LeetCode 練習專案

---
+++ LeetCode/Quez/121_Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,6 @@
+﻿int[] nums = new int[2];
+
+for (int i = 0; i < nums.Length; i++)
+{
+
+}
---
+++ LeetCode/template.cs
@@ -0,0 +1,19 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        List<int> list = new List<int>();
+        int[] nums = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+        }
+
+        while (true)
+        {
+
+        }
+    }
+}
