b5611c0f549cafdcb9ec6585adcf67c63b8e6003
KaChu234 <kachu234@ton-wa.com>
2024-05-31 10:27:58 +0800

    Add 217. Contains Duplicate

---
+++ LeetCode/Quez/217_Contains Duplicate.cs
@@ -0,0 +1,14 @@
+﻿//217. Contains Duplicate
+
+internal partial class Program
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> hashSet = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            hashSet.Add(nums[i]);
+        }
+        return nums.Length != hashSet.Count();
+    }
+}
